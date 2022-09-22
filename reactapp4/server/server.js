0c72ec23ad27afb6f52d0ef7185a15c3fb376118
Kai <kimobrie@ucsc.edu>
2022-09-22 02:40:04 -0700

    got server running!

--- reactapp4/server/server.js
+++ reactapp4/server/server.js
@@ -1,8 +1,13 @@
+
+/*
+* code taken from mongodb's setup 
+* https://www.mongodb.com/languages/mern-stack-tutorial
+*/
 const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 27017;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
