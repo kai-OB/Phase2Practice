cf99dc845fcc4d894ecfd046dc5765e50b4d14ff
Kai <kimobrie@ucsc.edu>
2022-09-21 21:50:28 -0700

    idk

---
+++ reactapp3/Components/Section/content.js
@@ -0,0 +1,35 @@
+/*
+* Section is a section of text
+* An optional title and an optional Description
+*/
+import React from 'react';
+import {Text, View} from 'react-native';
+import styles from './styles';
+
+const Section = ({children, title}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+export default Section;
--- reactapp3/__tests__/App-test.js
+++ reactapp3/__tests__/App-test.js
@@ -1,14 +1,16 @@
 /**
- * @format
+ * @jest-environment jsdom
  */
-
+//needed to specify because it didnt like render
 import 'react-native';
 import React from 'react';
 import App from '../App';
-
+import "@testing-library/jest-dom";
+import ReactDOM from "react-dom";
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import {render, fireEvent, waitFor, screen} from '@testing-library/react'
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+ renderer.create(<App />);
 });
